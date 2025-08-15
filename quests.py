6c97f7da16dc52a5d10691cdfdb63bb1cf28e586
Oleksandr <Oleksandr.Vasylenko@ibvcloud.eu>
2025-08-15 11:55:28 +0300

    updated quest emojis

--- quests.py
+++ quests.py
@@ -8,7 +8,7 @@ MAIN_QUESTS = [
                 "outcome": "The obese goat laughs hysterically while sheepishly trying to cover his noticeable boner. He tosses a coin your way.", "reward": "coins"},
             {"text": "📣 Tell him jokes about bones.",
                 "outcome": "Your jokes bring up unpleasant memories about his ex-wife. He looks sadly at the flowers.", "reward": "none"},
-            {"text": "🪨 Refuse to be his jester and offer something else.",
+            {"text": "🪑 Refuse to be his jester and offer something else.",
                 "outcome": "`Oh, I have a better use for you indeed!` The next instant you get tied up and put between the seat and Asgore's massive bare ass. You won't see the light of day for hours.", "reward": "mute"}
         ]
     },
@@ -17,11 +17,11 @@ MAIN_QUESTS = [
         "title": "Laundry day",
         "description": "Its a laundry day and this time it's your turn to wash yours and your roommates' underwear. You open the laundry box and get hit with a putrid stench of unwashed male musk. You get an instant boner. Even if used underwear was not your kink, now it is. The desire to grab a pair and take a long whiff is too strong. Which one would you pick?",
         "options": [
-            {"text": "💰 Torn tighty whites soaked with farts after weeks of abuse by your 400 lbs bear DnD nerd roommate",
+            {"text": "🩲 Torn tighty whites soaked with farts after weeks of abuse by your 400 lbs bear DnD nerd roommate",
                 "outcome": "You press your nose right in the middle of the skid mark and make a long whiff. Your brain instantly shuts down at the potent stench of unwashed ass. Each breath makes you dumber, more obedient, more addicted. After you came buckets sucking on the used pair of ruined undies, the decision was made. You can't live without that stinky nerd! You proposed to your slob roommate right in the middle of his Marvel Rivals match. You became his housewife: you cook for him, do the dishes and suck him off during long DnD sessions. But at least you don't have to work and the nerd is actually a rich dude and a nice grateful husband!", "reward": "coins"},
-            {"text": "🐽 Stretched out and damn jockstrap of your 7 ft tall gymrat tiger roommate",
+            {"text": "🏋️ Stretched out and damn jockstrap of your 7 ft tall gymrat tiger roommate",
                 "outcome": "`Well well, what do we have here?` - before you could even take your first whiff, you felt a powerful slap on your butt. A massive tiger emptied the whole laundry basket into the washing machine and then grabbed you and pushed your head inside. The smell of all the used clothes is overwhelming and your body gets limb so you cant protest when the jock pulls down your pants and starts breeding your virgin ass raw. `Come on, perv, make some noise for me! Say something like 'Aaah big brother help me, I'm stuuuck!' Bhahahaha! What a loser!`. After multiple rounds and what felt like hours of breeding, the tiger was satisfied and he wiped his fat uncut slab of meat with your tail, leaving you gaping and oozing with cum stuck in the washing machine.", "reward": "mute"},
-            {"text": "🧎 Your own underwear",
+            {"text": "🐽 Your own underwear",
                 "outcome": "The pleasant smell that you're so familiar with... After a quick wank you clean your junk with your own piece of clothing and empty the laundry basket into the washing machine. That's it.", "reward": "none"}
         ]
     },
@@ -43,11 +43,11 @@ MAIN_QUESTS = [
         "title": "Hypnotic massage",
         "description": "Tom Nook has given you a promo code for a free massage. You enter the facility to see a giant badger in his daddy age inviting you to lay on the table. `What is it bothering you?` - he asks calmly when you lay down.",
         "options": [
-            {"text": "📦 My shoulders are tired after work.",
+            {"text": "😖 My shoulders are tired after work.",
                 "outcome": "You get the best massage in your life. His strong manly hands make the pain melt away and you completely relax. Unfortunately that's about it.", "reward": "none"},
-            {"text": "👅 Family is stressing me out, I can't relax.",
+            {"text": "😫 Family is stressing me out, I can't relax.",
                 "outcome": "His gentle hands start rubbing your back and his moves quickly put you to sleep. `I am your family...` - You open your eyes to see yourself in a bed with the badger but he's older, he's got a grey beard and a large beer belly. You look around to see romantic pictures of yourself with this man that you barely know... Actually, you do know! You've been married to him for 41 years since he proposed to you after a few massages. All this time you've been in a deep trance like his puppet... But hey, your husband is rich!", "reward": "coins"},
-            {"text": "💬 I can't sleep because of the pain in my chest.",
+            {"text": "😴 I can't sleep because of the pain in my chest.",
                 "outcome": "The badger rolls you over and before you could protest, he pulls down his pants revealing his massive sweaty behind and plops his whole weight on your head and chest. Your chest pain is miraculously gone just like your dignity when you start eating out the fat badger's ass making him moan and purr. `I should cancel my next clients, Im going to need you for a few hours~`", "reward": "mute"}
         ]
     },
@@ -56,11 +56,11 @@ MAIN_QUESTS = [
         "title": "Personal kinky trainer",
         "description": "You enter the gym to meet your personal trainer for the first time. A big young muscular husky with just a bit of pudge over his prominent six-pack. He's blue eyes are so beautiful that you can't look away. `So tell me, cutie. What are your intentions with me?~ I mean what do you exercise for?`",
         "options": [
-            {"text": "🧎 I want to gain strength to be able to lift my partner and do more kinky sex poses.",
+            {"text": "💪 I want to gain strength to be able to lift my partner and do more kinky sex poses.",
                 "outcome": "The trainer shows you around and helps you learn how to use the gym equipment. You will get stronger for sure!", "reward": "none"},
-            {"text": "🎭 I want to improve my stamina so i could eat out my fat boyfriend's ass longer.",
+            {"text": "😤 I want to improve my stamina so i could eat out my fat boyfriend's ass longer.",
                 "outcome": "`Oh yeah? Well get down on the bench, dork!` - the husky pushes you to lay down and then he started squatting over your face. His massive muscular ass covered with tight sweaty shorts engulfed your face completely making it impossible to breath. `I think about a 100 sets should be enough` - the husky kept lifting the weights while you're choking on his ass for hours.", "reward": "mute"},
-            {"text": "👂 I want to be like you...",
+            {"text": "🪞 I want to be like you...",
                 "outcome": "You look the husky deep into his eyes. They are so blue, like the ocean itself — and you wanted to drown in them. `Bro, you are just like me! Aren't we twins?` - he turns you around and you see two almost identical buff huskies just the clothing is a bit different. You touch your new massive pecks, you slap your own huge meaty ass and flex your watermelon-sized biceps. You pull your tight gym shorts to take a peek inside and your jaw drops at the size of the monster cock that's at least 10 inches when flaccid and as thick as your wrist used to be. Your twin brother flexes with you and laughs - `Come on, bro, quit messing around! We got a lot of fresh nerds to teach how to lift!` You got a new body, a new life, a whole new identity. And your new job as a personal trainer is well-paid!", "reward": "coins"}
         ]
     },
